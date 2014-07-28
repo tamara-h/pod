baa1de6fe0e5eaa52a7aa24c321007372c3c960a
Ashley Davies-Lyons <ashley@daviesl.uk>
2014-07-28 15:13:22 +0100

    Server working

--- Python Server/Server.py
+++ Python Server/Server.py
@@ -6,11 +6,10 @@ def hello_world_app(environ, start_response):
     status = '200 OK' # HTTP Status
     headers = [('Content-type', 'text/plain; charset=utf-8')] # HTTP Headers
     start_response(status, headers)
-    '''
+    
     mc_conn = JAPI.Connection()    
     api = JAPI.JSONAPI(mc_conn)
-    api.players.name.ban("exterminationguy", "Test")
-    '''
+    
     print("Request URL {0}".format(serv.util.request_uri(environ)))    
     return [b"If this text displays, server was accessed successfully"]
 
