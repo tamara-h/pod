f1116e90de1d97add084b08acb494399e0c62301
Ashley Davies-Lyons <ashley@daviesl.uk>
2014-07-29 11:58:43 +0100

    Tidy-up and making output look a bit more bearable

--- Python Server/Server.py
+++ Python Server/Server.py
@@ -3,6 +3,8 @@ import wsgiref as serv
 import API2 as JAPI
 import random
 
+
+
 def request(environ, start_response):
     status = '200 OK' # HTTP Status
     headers = [('Content-type', 'text/plain; charset=utf-8')] # HTTP Headers
@@ -11,23 +13,12 @@ def request(environ, start_response):
     mc_conn = JAPI.Connection()    
     api = JAPI.JSONAPI(mc_conn)
 
-    print("Request URL {0}".format(serv.util.request_uri(environ)))
+    #print("Request URL {0}".format(serv.util.request_uri(environ)))
     requestURL = serv.util.request_uri(environ)
-    requestURL = requestURL[7:]
-    print(requestURL)
-    num = 1
-    # Could use partition but may mess up in edge cases like "http://" being omitted from request string
-    for char in requestURL:
-        if char.isnumeric() or char == "." or char == ":":
-            num += 1
-            continue
-        else:
-            break
-
-    requestURL = requestURL[num:].lower()
-    print(requestURL)
+    
     # Should have a string such as x=y
-    rURLComponents = requestURL.partition("=")
+    rURLComponents = requestURL.rpartition("/")[2].partition("=")
+    
     # We should have ['x', '=', 'y']
     var = rURLComponents[0]
     val = rURLComponents[2]
@@ -44,37 +35,43 @@ def request(environ, start_response):
 
 
 def setTemperature(api, temp):
-    print("Temperature is: " + temp)
+    print("[World Status Update] Temperature is: " + temp)
+    
 
 def setWeather(api, weather):
-    print("Weather is: " + weather)
+    print("[World Status Update] Weather is: " + weather)
     if "rain" in weather:
         print("Rainy")
         api.server.run_command("weather rain")
     else:
         print("Sunny")
-        api.server.run_comment("weather sun")
+        api.server.run_command("weather sun")
 
 # Abstractions
 def openWindows(api):
-    print("Opening Windows")
+    print("[House Change] Opening Windows")
     setWindows(api, "air")
+    
 def closeWindows(api):
-    print("Closing Windows")
+    print("[House Change] Closing Windows")
     setWindows(api, "glass")
 
+
 def lightFire(api):
-    print("Lighting fire")
-    enableRedstone(api, "122 71 97")
-    disableRedstone(api, "122 71 97")
+    print("[House Change] Lighting fire")
+    enableRedstone(api, "119 71 97")
+    disableRedstone(api, "119 71 97")
+
 
 # Backend for abstractions
 def enableRedstone(api, position):
-    print("Enabling redstone at " + position)
+    print("[House Change] Enabling redstone at " + position)
     setBlocks(api, "redstone_torch", [position])
+
 def disableRedstone(api, position):
-    print("Disabling redstone at " + position)
+    print("[House Change] Disabling redstone at " + position)
     setBlocks(api, "air", [position])
+
 
 def setWindows(api, windowType):
     windows = ["119 72 99", "119 72 100", "118 72 100", "117 72 100", "116 72 100", "115 72 100"]
@@ -82,11 +79,10 @@ def setWindows(api, windowType):
 
 def setBlocks(api, blockType, blockList):
     for block in blockList:
-        print("setblock " + block + " minecraft:" + blockType)
         api.server.run_command("setblock " + block + " minecraft:" + blockType)
     
 httpd = make_server('', 8000, request)
-print("Serving on port 8000...")
+print("[Online] Awaiting requests on port 8000")
 
 # Serve until process is killed
 httpd.serve_forever()
