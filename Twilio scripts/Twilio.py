2b42e16a137cc2aa55dbadd319bbace9304fd9d2
tamara-h <tamarah_@hotmail.co.uk>
2014-07-30 10:29:45 +0100

    ~*~*~*~*~*~

--- Twilio scripts/Twilio.py
+++ Twilio scripts/Twilio.py
@@ -23,6 +23,18 @@ def temp():
 		from_="+441631402052", 
 		body="Caution! Your house is reaching either the max/min temperature. Suggesion: either turn it up or down. xoxo, Pod",  
 	)
+	
+def flood():
+	ACCOUNT_SID = "AC42e4c31d0bb66d30387c967b1b8ebc61" 
+	AUTH_TOKEN = "0de403b3b1383b04811db2ad769a646d" 
+	 
+	client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN) 
+	 
+	client.messages.create(
+		to="+447476915987", 
+		from_="+441631402052", 
+		body="Warning! Water levels are running high in your area. Suggestion: flee! Every Pod for themselves \o/",  
+	)	
 
 
 def rain():
