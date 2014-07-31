07d883f1caf0787a458cde156f0287a0932a2607
Ashley Davies-Lyons <ashley@daviesl.uk>
2014-07-31 12:59:15 +0100

    Upgraded server; supports power
    Twilio calls also adjusted to give more information

--- Python Server/STwilio.py
+++ Python Server/STwilio.py
@@ -2,10 +2,11 @@ from twilio.rest            import TwilioRestClient
 
 class TwilioMessages():
     prefix          = ""
-    suffix          = "xoxo Pod"
+    suffix          = "xoxo, Pod"
     flood           = "Warning! Water levels are running high in your area. Suggestion: flee! Every Pod for themselves \o/"
     rain            = "Attention! It is raining. Suggestion: take washing inside."
-    tempWarn        = "Caution! Your house is reaching either the max/min temperature. Suggestion: either turn it up, or turn it down."
+    tempHighWarn    = "Caution! Your house is at, or close to, the maximum temperature. Suggestion: turn it down."
+    tempLowWarn     = "Caution! Your house is at, or close to, the minimum temperature. Suggestion: turn it up."
     doorLeftOpen    = "Bad news, your front door has been left unlocked. Suggestion: lock it."
     extremeHeat     = "Your house is above 3,000*C. Looks like you annoyed the Russians. Pod has not yet been upgraded to allow it to accurately give advice for nuclear wars, but we'll be sure to inform you when it can!"
     
@@ -13,8 +14,8 @@ class TwilioMessages():
     
 
 class TwilioClient():
-    enabled = False
-    template = "{} xoxo Pod"
+    enabled = True
+    template = "{} xoxo, Pod"
     
     def __init__(self):
         self.ACCOUNT_SID = "AC42e4c31d0bb66d30387c967b1b8ebc61"
